74db0cbb949cd78445d1db6e5ea240e656a1bbf0
Boi <boi@Bois-Mac.local>
2018-04-06 10:20:04 +0700

    NHOM 4

--- quizApp_G4/ViewController.swift
+++ quizApp_G4/ViewController.swift
@@ -9,7 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let questions = ["Thu cung yeu thich?","Mau sac yeu thich","Noi sinh o dau"]
+    let answers = [["Cho","Meo","Chim"],["Den","Trang","Do"],["Vung Tau","Ho Chi Minh","Ha Noi"]]
+    
+    var currentQuestion = 0
+    var rightAnswerPlacement:UInt32 = 0
 
+    @IBOutlet weak var lbl: UILabel!
+    
+    
+    @IBAction func action(_ sender: AnyObject)
+    {
+        if(sender.tag == Int(rightAnswerPlacement))
+        {
+            print ("RIGHT!!!!")
+        }
+        else
+        {
+            print ("WRONG!!!")
+        }
+        if(currentQuestion != questions.count)
+        {
+            newQuestion()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        newQuestion()
+    }
+    
+    func newQuestion()
+    {
+        lbl.text = questions[currentQuestion]
+        rightAnswerPlacement = arc4random_uniform(3)+1
+        
+        var button:UIButton = UIButton()
+        
+        var x = 1
+        
+        for i in 1...3
+        {
+            button = view.viewWithTag(i) as! UIButton
+            if(i == Int(rightAnswerPlacement))
+            {
+                button.setTitle(answers[currentQuestion][0], for: .normal)
+            }
+            else
+            {
+                button.setTitle(answers[currentQuestion][x], for: .normal)
+                x=2
+            }
+        }
+        currentQuestion += 1
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
